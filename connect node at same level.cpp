dd5e9a2babe60e5a814dee4149319a06f1feac03
vaibhavkaushik <vaibhavkaushik23@gmail.com>
2018-08-11 17:16:39 +0530

    Create connect node at same level.cpp

---
+++ connect node at same level.cpp
@@ -0,0 +1,113 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+  int data;
+  struct Node *left;
+  struct Node *right;
+  struct Node *nextRight;
+};
+void connect(struct Node *p);
+/* Helper function that allocates a new node with the
+   given data and NULL left and right pointers. */
+struct Node* newNode(int data)
+{
+  struct Node* node = new Node;
+  node->data = data;
+  node->left = NULL;
+  node->right = NULL;
+  return(node);
+}
+void printSpecial(Node *root)
+{
+   if (root == NULL)
+     return;
+   Node *node = root;
+   while (node != NULL)
+   {
+      printf("%d ", node->data);
+      node = node->nextRight;
+   }
+   if (root->left)
+     printSpecial(root->left);
+   else
+     printSpecial(root->right);
+}
+void inorder(Node *root)
+{
+    if (root == NULL)
+       return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+/* Driver program to test size function*/
+int main()
+{
+  int t;
+  scanf("%d
+", &t);
+  while (t--)
+  {
+     map<int, Node*> m;
+     int n;
+     scanf("%d",&n);
+     struct Node *root = NULL;
+     struct Node *child;
+     while (n--)
+     {
+        Node *parent;
+        char lr;
+        int n1, n2;
+        scanf("%d %d %c", &n1, &n2, &lr);
+        if (m.find(n1) == m.end())
+        {
+           parent = newNode(n1);
+           m[n1] = parent;
+           if (root == NULL)
+             root = parent;
+        }
+        else
+           parent = m[n1];
+        child = newNode(n2);
+        if (lr == 'L')
+          parent->left = child;
+        else
+          parent->right = child;
+        m[n2]  = child;
+     }
+     connect(root);
+     printSpecial(root);
+     printf("
+");
+     inorder(root);
+     printf("
+");
+  }
+  return 0;
+}
+
+
+// THIS IS THE MAIN FUNCTION THAT IS GOING DO THE JOB
+
+void connect(Node *p)
+{
+   if(!p)
+   return;
+   
+   if(p->left!=NULL)
+   p->left->nextRight = p->right;
+   
+   if(p->right!=NULL)
+   {
+       if(p->nextRight)
+       p->right->nextRight = p->nextRight->left;
+       else
+       p->right->nextRight = NULL;
+   }
+   
+   connect(p->left);
+   connect(p->right);
+       
+}
